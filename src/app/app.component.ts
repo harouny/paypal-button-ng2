07ea7da93af63ecebcb4b9078dafceb0a1650726
Ahmed Elharouny <Ahmed.Elharouny@Nintex.com>
2017-09-05 13:01:59 +1000

    add paypalBtn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  env = 'production'
+  client = {
+      sandbox:    'AWi18rxt26-hrueMoPZ0tpGEOJnNT4QkiMQst9pYgaQNAfS1FLFxkxQuiaqRBj1vV5PmgHX_jA_c1ncL',
+      production: 'AVZhosFzrnZ5Mf3tiOxAD0M6NHv8pcB2IFNHAfp_h69mmbd-LElFYkJUSII3Y0FPbm7S7lxBuqWImLbl'
+  }
+  commit = true
+  public payment(data, actions) {
+      return actions.payment.create({
+          transactions: [
+              {
+                  amount: { total: '1.00', currency: 'USD' }
+              }
+          ]
+      });
+  }
+  public onAuthorize(data, actions) {
+      return actions.payment.execute().then(function() {
+          // Show a success page to the buyer
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import * as ngCore from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    window['paypal'].Button.driver('angular2', ngCore)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ ],
+  bootstrap: [AppComponent],
+  schemas: [ ngCore.CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
